a82e5c302dc07c24922df6e510495d8643ed8eed
Allan <55001421+allancodex@users.noreply.github.com>
2024-06-19 22:04:00 -0300

    Update ace of spades.py

--- ace of spades.py
+++ ace of spades.py
@@ -4,6 +4,7 @@ class Card():
   def __init__(self,suit,rank):
     self.suit = suit
     self.rank = rank
+  
   def __str__(self):
     return f'{self.rank["rank"]} of {self.suit}'
 
@@ -25,11 +26,11 @@ class Deck:
             {'rank':'J', 'value':10},
             {'rank':'Q', 'value':10},
             {'rank':'K', 'value':10},
-  ]
+    ]
     
-    for rank in ranks:
-      for suit in suits:
-        self.cards.append([Card(suit, rank)])
+    for suit in suits:
+      for rank in ranks:
+        self.cards.append(Card(suit, rank))
   
   def shuffle(self):
     if len(self.cards) > 1:
@@ -37,13 +38,140 @@ class Deck:
   
   def deal(self, number):
     cards_dealt = []
-    
-    for card in range(number):
+    for x in range(number):
       if len(self.cards) > 0:
         card = self.cards.pop() 
         cards_dealt.append(card)    
     return cards_dealt
 
 class Hand:
-  def __init__(self):
+  def __init__(self, dealer=False):
     self.cards = []
+    self.value = 0
+    self.dealer = dealer
+
+  def add_card(self, card_list):
+    self.cards.extend(card_list)
+    
+  def calculate_value(self):
+    self.value = 0
+    has_ace = False
+    
+    for card in self.cards:
+      card_value = int(card.rank['value'])
+      self.value += card_value
+      if card.rank['rank'] == 'A':
+        has_ace = True
+
+    if has_ace and self.value > 21:
+      self.value -= 10
+
+  def get_value(self):
+    self.calculate_value()
+    return self.value
+
+  def is_black_jack(self):
+    return self.get_value == 21
+    
+  def display(self, show_all_dealer_cards = False):
+    print(f'''{"Dealer's" if self.dealer else "Your"} hand:''')
+    for index, card in enumerate(self.cards):
+      if index == 0 and self.dealer \
+      and not show_all_dealer_card and not self.is_black_jack:
+        print('hidden')
+      else:
+        print(card)
+
+    if not self.dealer:
+      print('Value:', self.get_value())
+    print()
+
+
+class Game:
+  def play(self):
+    game_number = 0
+    games2play = 0
+    
+    while games2play != int:
+      try:
+        games2play = int(input("Quantas rodadas voce quer jogar?"))
+        break 
+      except:
+        print('Insira um número')
+
+    while games_number < games2play:
+      game_number += 1
+      deck = Deck()
+      deck.shuffle()
+      player_hand = Hand()
+      dealer_hand = Hand(dealer = True)
+      
+      for i in range(2):
+        player_hand.add_card(deck.deal(1))
+        dealer_hand.add_card(deck.deal(1))
+      print()
+      
+      print(f'Game {game_number} of {games2play}')
+        print('*'*30)
+      player_hand.display()
+      dealer_hand.display()
+
+      if self.check_winner(player_hand, dealer_hand):
+        continue
+
+      choice = ''
+      while player_hand.get_value() < 21 and choice not in ['s', 'stand']:
+        choice = input('Please choose "Hit" or "Stand": ').lower    
+        print()
+        while choice not in ['hit','stand', 's','h']:
+          choice = input('Please choose "Hit(h)" or "Stand(s)"').lower
+          print()
+        
+        
+  
+  def check_winner(self, player_hand, dealer_hand, game_over = False):
+    if not game_over:
+      if player_hand.get_value() > 21:
+        print('You busted. Dealer wins!')
+        return True
+      elif dealer_hand > 21:
+        print('Dealer busted. You win')
+        return True
+      elif dealer_hand.is_blackjack() and player_hand.is_blackjack() :
+        print('Tie!')
+        return True
+      elif player_hand.is_blackjack():
+        print('You win')
+        return True
+      elif dealer_hand.is_blackjack():
+        print('Dealer wins')
+        return True
+    else:
+      if player_hand.get_value() > dealer_hand.get_value():
+        print('You win')
+      elif player_hand.get_value() == dealer_hand.get_value():
+        print('Tie')
+      else:
+        prin('Dealers wins')
+      return True
+    return False
+    
+g = Game()
+g.play()
+  
+
+  
+  
+
+
+
+
+
+
+  
+
+
+
+
+
+
